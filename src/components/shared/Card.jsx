ce1ddc56c246b48adf09f95d8ba7f870d4186139
Ian Cover <iancover@gmail.com>
2021-12-16 05:39:13 -0600

    feat: Add Card shared component to reuse styled card

---
+++ src/components/shared/Card.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types'
+
+
+function Card({ children, reverse }) {
+  // return <div className={`card ${reverse && 'reverse'}`}>{children}</div>;
+
+  return (
+    <div
+      className='card'
+      style={{
+        backgroundColor: reverse ? 'var(--grey-color)' : 'var(--lightblue-color)',
+        color: reverse ? 'var(--light-color)' : 'var(--grey-color)'
+      }}>
+      {children}
+    </div>
+  );
+}
+
+Card.defaultProps = {
+  reverse: false
+}
+
+Card.propTypes = {
+  children: PropTypes.node.isRequired,
+  reverse: PropTypes.bool,
+}
+
+export default Card;
