557a5b96a6554e6f16221da9a762c84a6914e041
Ian Cover <iancover@gmail.com>
2021-12-16 04:12:37 -0600

    style: Change title caps size start and end letters

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 
-function Header({ title, bgColor, txtColor }) {
+function Header({ titleStart, titleEnd, bgColor, txtColor }) {
   const headerStyles = {
     backgroundColor: bgColor,
     color: txtColor
@@ -9,20 +9,22 @@ function Header({ title, bgColor, txtColor }) {
   return (
     <header style={headerStyles}>
       <div className='container'>
-        <h1 className='title'>{title}</h1>
+        <h1 className='title'>{titleStart}<span className='title'>{titleEnd}</span></h1>
       </div>
     </header>
   );
 }
 
 Header.defaultProps = {
-  title: 'CRITIQ',
+  titleStart: 'crit',
+  titleEnd: 'IQ',
   bgColor: 'var(--grey-color)',
   txtColor: 'var(--orange-color)'
 };
 
 Header.propTypes = {
-  text: PropTypes.string.isRequired,
+  titleStart: PropTypes.string.isRequired,
+  titleEnd: PropTypes.string.isRequired,
   bgColor: PropTypes.string.isRequired,
   txtColor: PropTypes.string.isRequired
 };
