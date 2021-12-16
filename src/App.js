89ad7e03e8f28e9d37ea521d9adf355c1137fec6
Ian Cover <iancover@gmail.com>
2021-12-16 02:08:23 -0600

    feat: Add ReviewItem and modify colors index.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Header from './components/Header';
+import ReviewItem from './components/ReviewItem';
 
 function App() {
   return (
     <>
       <Header />
       <div className={'container'}>
-        <h1>My App</h1>
+        <ReviewItem />
       </div>
     </>
   );
---
+++ src/components/ReviewItem.jsx
@@ -0,0 +1,10 @@
+function ReviewItem() {
+  return (
+    <div className='card'>
+      <div className='num-display'>10</div>
+      <div className='text-display'>This is an example of a Review Item</div>
+    </div>
+  );
+}
+
+export default ReviewItem;
