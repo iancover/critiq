bcc0f9e995c24bd87c50fb77ae2c6c62e06559fd
Ian Cover <iancover@gmail.com>
2021-12-17 05:20:12 -0600

    refactor: Change styles passed as const into Card

--- src/components/shared/Card.jsx
+++ src/components/shared/Card.jsx
@@ -1,28 +1,26 @@
-import PropTypes from 'prop-types'
-
+import PropTypes from 'prop-types';
 
 function Card({ children, reverse }) {
-  // return <div className={`card ${reverse && 'reverse'}`}>{children}</div>;
+  const cardStyles = {
+    backgroundColor: reverse ? 'var(--grey-color)' : 'var(--lightblue-color)',
+    color: reverse ? 'var(--light-color)' : 'var(--grey-color)',
+  };
 
+  // return <div className={`card ${reverse && 'reverse'}`}>{children}</div>;
   return (
-    <div
-      className='card'
-      style={{
-        backgroundColor: reverse ? 'var(--grey-color)' : 'var(--lightblue-color)',
-        color: reverse ? 'var(--light-color)' : 'var(--grey-color)'
-      }}>
+    <div className='card' style={cardStyles}>
       {children}
     </div>
   );
 }
 
 Card.defaultProps = {
-  reverse: false
-}
+  reverse: false,
+};
 
 Card.propTypes = {
   children: PropTypes.node.isRequired,
   reverse: PropTypes.bool,
-}
+};
 
 export default Card;
