5d5a3c9ff5a28d3aeea1bf401a6d1a4bcc100d94
Ian Cover <iancover@gmail.com>
2021-12-21 06:02:53 -0600

    feat: Add NavLink with new react-router props
    With React Router v6 NavLink components dont use the activeClassName
    or activeStyle props anymore, instead just use regular
    style and color props with isActive boolean

--- src/components/pages/AboutPage.jsx
+++ src/components/pages/AboutPage.jsx
@@ -1,19 +1,43 @@
 import Card from '../shared/Card';
-import { Link } from 'react-router-dom';
+import { NavLink, useMatch } from 'react-router-dom';
 
 function AboutPage() {
-  return (
-    <Card>
-      <div className={'about'}>
-        <h1>About critIQ</h1>
-        <p>This is a React App to rate and review products originally from a Udemy course.</p>
-        <p>Version: 1.0.0</p>
+  let match = useMatch('/about');
 
-        <p>
-          <Link to={'/'}>Home</Link>
-        </p>
-      </div>
-    </Card>
+  console.log(match);
+
+  return (
+    <>
+      <Card>
+        <div className={'about'}>
+          <h1>About critIQ</h1>
+          <p>This is a React App to rate and review products originally from a Udemy course.</p>
+          <p>Version: 1.0.0</p>
+        </div>
+      </Card>
+      <Card>
+        <nav>
+          <NavLink
+            to={'/'}
+            className={({ isActive }) => 'nav-link' + (isActive ? ' active' : '')}
+            style={({ isActive }) => ({ color: isActive ? 'var(--orange-color)' : 'var(--green-color)' })}>
+            Home
+          </NavLink>
+          <NavLink
+            to={'/post'}
+            className={({ isActive }) => 'nav-link' + (isActive ? ' active' : '')}
+            style={({ isActive }) => ({ color: isActive ? 'var(--orange-color)' : 'var(--green-color)' })}>
+            Post
+          </NavLink>
+          <NavLink
+            to={'.'}
+            className={({ isActive }) => 'nav-link' + (isActive ? ' active' : '')}
+            style={({ isActive }) => ({ color: isActive ? 'var(--orange-color)' : 'var(--green-color)' })}>
+            About
+          </NavLink>
+        </nav>
+      </Card>
+    </>
   );
 }
 
