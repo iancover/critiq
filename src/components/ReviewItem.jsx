85b0647ff5942909e9df2050e81a742adc12df23
Ian Cover <iancover@gmail.com>
2021-12-21 02:33:59 -0600

    feat: Add edit icon and tooltips

--- src/components/ReviewItem.jsx
+++ src/components/ReviewItem.jsx
@@ -1,4 +1,4 @@
-import { MdClose } from 'react-icons/md';
+import { MdEditNote as EditIcon, MdDelete as DeleteIcon } from 'react-icons/md';
 import PropTypes from 'prop-types';
 import Card from './shared/Card';
 
@@ -6,8 +6,13 @@ function ReviewItem({ item, handleDelete }) {
   return (
     <Card>
       <div className='num-display'>{item.rating}</div>
+      <button className='edit' onClick={() => console.log('edit')}>
+        <EditIcon />
+        <span className={'tooltip'}>Edit</span>
+      </button>
       <button className='close' onClick={() => handleDelete(item.id)}>
-        <MdClose color='var(--grey-color)' />
+        <DeleteIcon />
+        <span className={'tooltip'}>Delete</span>
       </button>
       <div className='text-display'>{item.text}</div>
     </Card>
