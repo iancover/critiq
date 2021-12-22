7c4b2e8ac10cf748263a5037dddceaa8c4021ab9
Ian Cover <iancover@gmail.com>
2021-12-22 00:45:28 -0600

    feat: Add useParams hook
    With useParams hook we can use the params passed from the Route path
    in App.js <Route path='/post/:id/:name' element={<Post />} />

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,14 @@
-import { useParams } from "react-router-dom";
+import { useParams } from 'react-router-dom';
 
 function Post() {
-  return <div>Post</div>;
+  const params = useParams();
+
+  return (
+    <div>
+      <h1>Post {params.id}</h1>
+      <p>Name: {params.name.charAt(0).toUpperCase() + params.name.slice(1)}</p>
+    </div>
+  );
 }
 
 export default Post;
