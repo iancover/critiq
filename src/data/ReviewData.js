2d3dcf798f9069c864adf8368c747ca4be20f847
Ian Cover <iancover@gmail.com>
2021-12-23 02:06:25 -0600

    Update text

--- src/data/ReviewData.js
+++ src/data/ReviewData.js
@@ -3,22 +3,22 @@ const ReviewData = [
   {
     id: 1,
     rating: 10,
-    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, dolorum fuga. Nihil odit tempore veniam quod in magnam ratione pariatur!'
+    text: 'Uno Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, dolorum fuga. Nihil odit tempore veniam quod in magnam ratione pariatur!'
   },
   {
     id: 2,
     rating: 9,
-    text: 'Ipsum dolor lorem sit amet odit tempore veniam quod in magnam ratione pariatur! consectetur adipisicing elit. Inventore'
+    text: 'Dos Ipsum dolor lorem sit amet odit tempore veniam quod in magnam ratione pariatur! consectetur adipisicing elit. Inventore'
   },
   {
     id: 3,
     rating: 7,
-    text: 'Dolor lorem ipsum, dolor sit amet consectetur adipisicing elit. dolor sit amet dolor sit amet Asperiores minus, laboriosam ullam alias fugit natus.'
+    text: 'Tres Dolor lorem ipsum, dolor sit amet consectetur adipisicing elit. dolor sit amet dolor sit amet Asperiores minus, laboriosam ullam alias fugit natus.'
   },
   {
     id: 4,
     rating: 8,
-    text: 'Lorem ipsum, Asperiores minus, laboriosam dolor sit amet consectetur adipisicing elit.  ullam alias fugit natus.'
+    text: 'Cuatro Lorem ipsum, Asperiores minus, laboriosam dolor sit amet consectetur adipisicing elit.  ullam alias fugit natus.'
   }
 ];
 
