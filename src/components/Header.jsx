bdde753a03b9f556f0f1a3e343a596a9f8d901cf
Ian Cover <iancover@gmail.com>
2022-06-23 17:54:17 -0500

    refactor: Redirect home clicking the logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,21 @@
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 function Header({ titleStart, titleEnd, bgColor, txtColor }) {
   const headerStyles = {
     backgroundColor: bgColor,
-    color: txtColor
+    color: txtColor,
   };
 
   return (
     <header style={headerStyles}>
       <div className='container'>
-        <h1 className='title'>{titleStart}<span className='title'>{titleEnd}</span></h1>
+        <Link to='/' style={{ textDecoration: 'none', color: 'inherit' }}>
+          <h1 className='title'>
+            {titleStart}
+            <span className='title'>{titleEnd}</span>
+          </h1>
+        </Link>
       </div>
     </header>
   );
@@ -19,14 +25,14 @@ Header.defaultProps = {
   titleStart: 'crit',
   titleEnd: 'IQ',
   bgColor: 'rgba(0,0,0,0.4)',
-  txtColor: 'var(--orange-color)'
+  txtColor: 'var(--orange-color)',
 };
 
 Header.propTypes = {
   titleStart: PropTypes.string.isRequired,
   titleEnd: PropTypes.string.isRequired,
   bgColor: PropTypes.string.isRequired,
-  txtColor: PropTypes.string.isRequired
+  txtColor: PropTypes.string.isRequired,
 };
 
 export default Header;
