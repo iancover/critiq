f9c849b5952762d684485e95780b147b45fce75f
Ian Cover <iancover@gmail.com>
2022-06-23 17:45:09 -0500

    refactor: Update to ReactDOM v18
    - Remove <StrictMode> react component and add the ReactDOM
      function createRoot() before the App in render()

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
-import { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 // css
 import './index.css';
 // App
 import App from './App';
 
-ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
