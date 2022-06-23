a227b6563399ec3c9000e1e9b29808be299ad879
Ian Cover <iancover@gmail.com>
2022-06-23 18:37:29 -0500

    fix: Pass the callback funtion inside the .from()
    - Previously had Array.from( obj ), () => and needed to pass the
      cb fn inside, so the rating inputs were not displaying.
      Array.from( obj, () => )

--- src/components/RatingSelect.jsx
+++ src/components/RatingSelect.jsx
@@ -9,24 +9,19 @@ function RatingSelect({ selectRating, selectedRating }) {
   // NOTES: add iteration instead
   return (
     <ul className='rating'>
-      {
-        (Array.from({ length: 10 }),
-        (_, i) => (
-          <>
-            <li key={`rating-${i + 1}`}>
-              <input
-                type='radio'
-                id={`num${i + 1}`}
-                name='rating'
-                value={i + 1}
-                onChange={handleChange}
-                checked={selectedRating === i + 1}
-              />
-              <label htmlFor={`num${i + 1}`}>{i + 1}</label>
-            </li>
-          </>
-        ))
-      }
+      {Array.from({ length: 10 }, (_, i) => (
+        <li key={`rating-${i + 1}`}>
+          <input
+            type='radio'
+            id={`num${i + 1}`}
+            name='rating'
+            value={i + 1}
+            onChange={handleChange}
+            checked={selectedRating === i + 1}
+          />
+          <label htmlFor={`num${i + 1}`}>{i + 1}</label>
+        </li>
+      ))}
     </ul>
   );
 }
